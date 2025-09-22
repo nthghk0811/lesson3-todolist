7a4f7a7012e712bd8bfbbc9e89ee102bc1e3745f
nthghk0811 <nguyenhoanghai811@gmail.com>
2025-09-23 01:57:54 +0700

    search

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,33 @@ import './App.css'
 import {tasks} from './constant'
 import {Cart} from './components/Cart'
 import ColumnItems from './components/ColumnItems'
-function App() {
-  
 
+
+function App() {
+  const [listTask, setListTask] = useState(tasks)
+  const onChange = (e) => {
+    //search
+    const findItem = tasks.filter((task) => {
+    const findTitle = task.title.toLowerCase().includes(e.target.value.toLowerCase())
+    
+    const findDes = task.description.toLowerCase().includes(e.target.value.toLowerCase())
+    return findTitle || findDes
+  })
+  setListTask(findItem)
+  }
+
+  
   return (
     <div className="container" style={{padding: '8px'}}>
       <div className="header">
-        <input type="text" placeholder="Search" />
+        <input type="text" onChange={onChange} placeholder="Search" />
         <button>New Item</button>
       </div>
       <div className="mainContent">
-        <ColumnItems listTask={tasks} name="TO DO" statusId={1}/>
-        <ColumnItems listTask={tasks} name="IN PROGRESS" statusId={2}/>
-        <ColumnItems listTask={tasks} name="IN REVIEW" statusId={3}/>
-        <ColumnItems listTask={tasks} name="DONE" statusId={4}/>
+        <ColumnItems listTask={listTask} name="TO DO" statusId={1}/>
+        <ColumnItems listTask={listTask} name="IN PROGRESS" statusId={2}/>
+        <ColumnItems listTask={listTask} name="IN REVIEW" statusId={3}/>
+        <ColumnItems listTask={listTask} name="DONE" statusId={4}/>
       </div>
       
       </div>
